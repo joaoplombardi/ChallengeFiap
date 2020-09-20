b7cf37611e1a4a2aa29e8443114bd9fe598adfa9
Bruno Yudi Tiyoda <bruno_tiyoda@hotmail.com>
2020-09-20 14:36:33 -0300

    updates

--- src/br/com/fiap/b2w/App.java
+++ src/br/com/fiap/b2w/App.java
@@ -1,26 +1,24 @@
 package br.com.fiap.b2w;
 
-import br.com.fiap.b2w.dao.AssociadoDAO;
-import br.com.fiap.b2w.dao.GestorDAO;
 import br.com.fiap.b2w.models.*;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-    public static void menu(){
+    public static void menu() {
         System.out.println(" -------------------------------------");
-        System.out.println("|                SOG                  |");
+        System.out.println("|                SGP                  |");
         System.out.println("| 1-Dashboard do Gestor               |");
         System.out.println("| 2-Dashboard do RH                   |");
         System.out.println("| 3-Dashboard do Associado            |");
         System.out.println("| 0-Sair                              |");
         System.out.println(" -------------------------------------");
     }
-    public static void menuGestor(){
+
+    public static void menuGestor() {
         System.out.println(" -------------------------------------");
         System.out.println("|                SGP                  |");
         System.out.println("| 1-Criar Plano de Desenvolvimento    |");
@@ -28,17 +26,19 @@ public class App {
         System.out.println("| 0-Sair                              |");
         System.out.println(" -------------------------------------");
     }
-    public static void menuRH(){
+
+    public static void menuRH() {
         System.out.println(" -------------------------------------");
-        System.out.println("|                SOG                  |");
+        System.out.println("|                SGP                  |");
         System.out.println("| 1-Aprovar Plano de Desenvolvimento  |");
         System.out.println("| 2-Consultar Planos da equipe        |");
         System.out.println("| 0-Sair                              |");
         System.out.println(" -------------------------------------");
     }
-    public static void menuAssociado(){
+
+    public static void menuAssociado() {
         System.out.println(" -------------------------------------");
-        System.out.println("|                SOG                  |");
+        System.out.println("|                SGP                  |");
         System.out.println("| 1-Consultar Plano de Desenvolvimento|");
         System.out.println("| 2-Iniciar Task                      |");
         System.out.println("| 3-Encerrar Task                     |");
@@ -54,12 +54,12 @@ public class App {
         opcao = scan.nextInt();
         scan.nextLine();
         do {
-            switch (opcao){
+            switch (opcao) {
                 case 1 -> dashboardGestor(scan);
                 case 2 -> dashboardRH(scan);
                 case 3 -> dashboardAssociado(scan);
             }
-        }while(opcao != 0);
+        } while (opcao != 0);
     }
 
     private static void dashboardAssociado(Scanner scan) {
@@ -76,29 +76,56 @@ public class App {
         Scanner scan = new Scanner(System.in);
         try {
             System.out.print("Qual o seu número de matricula: ");
-            Gestor gestor = new GestorDAO().consultaPorCodigo(scan.nextInt());
+            Gestor gestor = new Gestor();
+            gestor.setNomeCompleto("Bruno");
             scan.nextLine();
             System.out.print("Qual o número da matricula do Associado: ");
-            Associado associado = new AssociadoDAO().consultaPorCodigo(scan.nextInt());
+            Associado associado = new Associado();
+            scan.nextLine();
+
             Task task = new Task();
             task.setNome("Java");
             task.setStatus(Status.CRIADA);
+
+            Task task2 = new Task();
+            task2.setNome("Oracle");
+            task2.setStatus(Status.CRIADA);
+
             List<Task> arrayTask = new ArrayList<>();
             arrayTask.add(task);
+            arrayTask.add(task2);
+
             PlanodeDesenvolvimento planodeDesenvolvimento = new PlanodeDesenvolvimento();
+            planodeDesenvolvimento.setCdPlanodeDesenvolvimento(123);
             planodeDesenvolvimento.setTasks(arrayTask);
-            associado.setNomeCompleto("sergio");
+
+            associado.setNomeCompleto("Sérgio");
             associado.setPlanodeDesenvolvimento(planodeDesenvolvimento);
+
             List<PlanodeDesenvolvimento> arrayPlanosCriados = new ArrayList<>();
             arrayPlanosCriados.add(planodeDesenvolvimento);
             gestor.setPlanosCriados(arrayPlanosCriados);
-            System.out.println(task.getNome());
-            System.out.println(planodeDesenvolvimento.getAssociado().getNomeCompleto());
+
+
+            RH rh = new RH();
+            rh.setPlanosParaAprovacao(arrayPlanosCriados);
+
+            associado.setPlanodeDesenvolvimento(planodeDesenvolvimento);
+
+            //System.out.println(planodeDesenvolvimento.getAssociado().getNomeCompleto()); não faz sentido
+            System.out.println("Gestor..................: " + gestor.getNomeCompleto());
+            System.out.println("Planos Criados..........: " + gestor.getPlanosCriados().size());
+            gestor.getPlanosCriados().forEach(planoCriado ->
+                    System.out.println("Código do Plano.........: " + planoCriado.getCdPlanodeDesenvolvimento()));
+            gestor.getPlanosCriados().forEach(planoCriado ->
+                    planoCriado.getTasks().forEach(task1 -> System.out.println("Task do Plano...........: " + task1.getNome())));
+            System.out.println("Associado...............: " + associado.getNomeCompleto());
+            System.out.println("Plano com Associado.....: " + associado.getPlanodeDesenvolvimento().getCdPlanodeDesenvolvimento());
+            associado.getPlanodeDesenvolvimento().getTasks().forEach(task1 ->
+                    System.out.println("Task com Associado......: " + task1.getNome()));
 
             scan.nextLine();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         scan.close();
@@ -115,7 +142,7 @@ public class App {
         System.out.print("Qual ação será realizada: ");
         opcaoGestor = scan.nextInt();
         scan.nextLine();
-        switch (opcaoGestor){
+        switch (opcaoGestor) {
             case 1 -> criarPlanoDesenv();
             case 2 -> consultaPlanosGestor();
         }
